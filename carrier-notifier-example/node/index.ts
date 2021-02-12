7258ccf1a548e50c6c219dcd4ee88550c8b68249
Jose Iago Pereira Araujo <joseiago.p.a@gmail.com>
2021-02-12 00:19:23 -0300

    Add outbound-access example

--- carrier-notifier-example/node/index.ts
+++ carrier-notifier-example/node/index.ts
@@ -1,12 +1,25 @@
 import {
+  ClientsConfig,
   method,
   Service,
 } from '@vtex/api'
+import { Clients } from './clients'
 import { notfisFile } from './middlewares/notfisFile'
 import { notify } from './middlewares/notifier'
 
+const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: 2000,
+    },
+  },
+}
+
 // Export a service that defines route handlers and client options.
 export default new Service({
+  clients,
   routes: {
     // `status` is the route ID from service.json. It maps to an array of middlewares (or a single handler).
     notify: method({
--- carrier-notifier-example/node/middlewares/notifier.ts
+++ carrier-notifier-example/node/middlewares/notifier.ts
@@ -1,9 +1,13 @@
 import { json } from 'co-body'
 
 export async function notify(ctx: Context, next: () => Promise<any>) {
-
+  const { clients: { google } } = ctx
+  
   const content = await json(ctx.req) as NotificationRequest
   console.log(content)
+
+  const googleResponse = await google.Get()
+  console.log(googleResponse != null)
 
   ctx.status = 200
   ctx.body = {
